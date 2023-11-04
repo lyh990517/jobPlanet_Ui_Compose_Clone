3a5d64f77bd0cfbc551955ff9cb720cb617d41ea
lyh990517 <lyh990517@naver.com>
2023-11-04 16:43:44 +0900

    홈 화면 수정

--- app/src/main/java/com/example/jobplanetuiclone/screen/HomeScreen.kt
+++ app/src/main/java/com/example/jobplanetuiclone/screen/HomeScreen.kt
@@ -2,9 +2,15 @@ package com.example.jobplanetuiclone.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.jobplanetuiclone.ui_component.BannerRotation
 import com.example.jobplanetuiclone.ui_component.JobRecommendation
 import com.example.jobplanetuiclone.ui_component.JobTheme
@@ -16,12 +22,16 @@ import com.example.jobplanetuiclone.ui_component.TopMenu
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+    val scrollState = rememberScrollState()
     Scaffold {
-        Column {
+        Column(modifier = Modifier.verticalScroll(scrollState)) {
             TopMenu()
             BannerRotation()
+            Spacer(modifier = Modifier.height(10.dp))
             JobsMenu()
+            Spacer(modifier = Modifier.height(30.dp))
             JobRecommendation()
+            Spacer(modifier = Modifier.height(30.dp))
             JobTheme()
             ThemeRecommendation()
         }
