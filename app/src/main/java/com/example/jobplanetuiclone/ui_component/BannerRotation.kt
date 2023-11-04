fbdd8bd344f70b2a00bf273d2b0e8adbd3c521ca
lyh990517 <lyh990517@naver.com>
2023-11-04 16:42:03 +0900

    배너뷰 임시 구현

--- app/src/main/java/com/example/jobplanetuiclone/ui_component/BannerRotation.kt
+++ app/src/main/java/com/example/jobplanetuiclone/ui_component/BannerRotation.kt
@@ -1,8 +1,80 @@
 package com.example.jobplanetuiclone.ui_component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.jobplanetuiclone.R
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun BannerRotation() {
+    val pagerState = rememberPagerState(
+        initialPage = 10,
+    )
+    HorizontalPager(pagerState.currentPage) { page ->
+        Banner(page.toString())
+    }
+}
 
+@Composable
+fun Banner(text: String) {
+    Card(
+        modifier = Modifier
+            .height(150.dp)
+            .fillMaxWidth()
+            .padding(10.dp)
+            .clip(RoundedCornerShape(15.dp))
+            .background(Color.Gray)
+    ) {
+        Row(
+            Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(20.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "전 현직원 추천")
+                Text(text = "높은 연봉으로 보상받아요")
+                Text(text = "채용 공고 보기")
+            }
+            Image(
+                modifier = Modifier
+                    .padding(20.dp)
+                    .size(50.dp),
+                painter = painterResource(id = R.drawable.dummy_image),
+                contentDescription = "icon",
+                contentScale = ContentScale.Crop
+            )
+        }
+        Text(text = text)
+    }
 }
