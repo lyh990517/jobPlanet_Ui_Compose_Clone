d7780831861ce2e0c2d80ca36f516e1db74668f4
lyh990517 <lyh990517@naver.com>
2023-11-04 16:43:33 +0900

    제목뷰 구현

---
+++ app/src/main/java/com/example/jobplanetuiclone/ui_component/RowTitle.kt
@@ -0,0 +1,24 @@
+package com.example.jobplanetuiclone.ui_component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RowTitle(title: String, menu: String = "") {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(10.dp), verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(modifier = Modifier.weight(1f), text = title, fontSize = 17.sp)
+        Text(text = menu)
+    }
+    JobRow()
+}
