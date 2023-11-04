5314af194a7470b9c77dbeeea99da3b6c6918017
lyh990517 <lyh990517@naver.com>
2023-11-04 16:41:42 +0900

    상단 메뉴 임시 구현

--- app/src/main/java/com/example/jobplanetuiclone/ui_component/TopMenu.kt
+++ app/src/main/java/com/example/jobplanetuiclone/ui_component/TopMenu.kt
@@ -1,7 +1,9 @@
 package com.example.jobplanetuiclone.ui_component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
@@ -9,19 +11,37 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TopMenu() {
-    Row(Modifier.fillMaxWidth()) {
-        Text(text = "YoonPlanet", modifier = Modifier.padding(5.dp))
-        Row(Modifier.weight(1f)) {
-            Text(text = "2")
-            Text(text = "dummyCompany")
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(60.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "YoonPlanet", modifier = Modifier.padding(8.dp), fontSize = 23.sp)
+        Row(
+            Modifier.weight(1f),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(text = "2", Modifier.padding(5.dp))
+            Text(text = "dummyCompany", Modifier.padding(5.dp))
         }
-        Icon(imageVector = Icons.Default.Search, contentDescription = "search")
-        Icon(imageVector = Icons.Default.Notifications, contentDescription = "Notification")
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "search",
+            modifier = Modifier.padding(5.dp)
+        )
+        Icon(
+            imageVector = Icons.Default.Notifications,
+            contentDescription = "Notification",
+            modifier = Modifier.padding(5.dp)
+        )
     }
 }
 //416790*
